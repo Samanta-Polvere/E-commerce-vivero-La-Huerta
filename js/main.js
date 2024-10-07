da156e64bdf0422792168bcd7f21264dc32562d1
samantapolvere <samanta_polvere@hotmail.com>
2024-10-07 00:26:26 -0300

    corrigiendo continuar compra

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ if (registro === 1) {
     registrarse(nombre, apellido, mail, contraseña);
 
     alert("¡Hola " + nombre + " " + apellido + "!. ¡Ya sos parte de nuestra comunidad!");
+//logueo//
 } else if (registro === 2) {
 
 
@@ -37,179 +38,182 @@ if (registro === 1) {
     }
 
 }
-    const continuarCompra = parseInt(prompt("Si deseas continuar con la compra presioná 1, sino presioná 2 para salir del proceso de compra. "));
-  
-    const arraycategoria = ["Macetas", "Plantas", "Accesorios"];
+const continuarCompra = parseInt(prompt("Si deseas continuar con la compra presioná 1, sino presioná 2 para salir del proceso de compra. "));
 
-    const arraymacetas = ["Maceta Rotoldeadas", "Macetas animales", "Maceta Terracota Nro14"];
+const arraycategoria = ["Macetas", "Plantas", "Accesorios"];
 
-    const plantas = ["Orquidea Phalaenopsis", "Ficus Hawaii", "Gomero Rubra"];
+const arraymacetas = ["Maceta Rotoldeadas", "Macetas animales", "Maceta Terracota Nro14"];
 
-    const accesorios = ["Tutores de caña fina", "Figuras de mariposas decorativas", "Estanteria para plantas"];
+const plantas = ["Orquidea Phalaenopsis", "Ficus Hawaii", "Gomero Rubra"];
 
+const accesorios = ["Tutores de caña fina", "Figuras de mariposas decorativas", "Estanteria para plantas"];
+
+//realizar compra//
 if (continuarCompra === 1) {
     alert("Ya puedes comprar!");
-    let seleccionarCateg = parseInt(prompt("Accedé a la categoria que más te guste para poder ver los productos: Opción 1 Categoría: Plantas, Opcion 2 Categoría: Macetas y Opción 3 Categoría: Accesorios. "));
-    switch (seleccionarCateg) {
-        case 1:
-            let prodPlanta = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Orquidea Phalaenopsis , Opcion 2 Ficus Hawaii y Opción 3 Gomero Rubra. "));
-            switch (prodPlanta) {
-                case 1:
-                    alert("Seleccionaste Articulo Orquidea Phalaenopsis. Precio: $530");
-                    function comprarOrquidea() {
-                        let cantidad = parseInt(prompt("Cuántas Orquideas Phalaenopsis deseas? (Stock máximo 20)"));
+    let seleccionarCateg = parseInt(prompt("Accedé a la categoria que más te guste para poder ver los productos: Opción 1 Categoría: Plantas, Opcion 2 Categoría: Macetas y Opción 3 Categoría: Accesorios. Si deseas salir del proceso de compra, presioná 0 "));
+    while (seleccionarCateg != 0) {
+        switch (seleccionarCateg) {
 
-                        if (cantidad < 20) {
-                            alert("Agregaste al carrito " + cantidad + " Orquideas Phalaenopsis");
-                            alert("Precio final: $" + (530 * cantidad));
+            case 1:
+                let prodPlanta = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Orquidea Phalaenopsis , Opcion 2 Ficus Hawaii y Opción 3 Gomero Rubra. Si deseas salir del proceso de compra, presioná 0 "));
+                switch (prodPlanta) {
+
+                    case 1:
+                        alert("Seleccionaste Articulo Orquidea Phalaenopsis. Precio: $530");
+                        let cantidadOrquideas = parseInt(prompt("Cuántas Orquideas Phalaenopsis deseas? (Stock máximo 20)"));
+                        if (cantidadOrquideas < 20) {
+                            alert("Agregaste al carrito " + cantidadOrquideas + " Orquideas Phalaenopsis");
+                            alert("Precio final: $" + (530 * cantidadOrquideas));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarOrquidea();
-                    break;
 
-                case 2:
-                    alert("Seleccionaste Articulo Ficus Hawaii. Precio: $730");
-                    function comprarFicus() {
-                        let cantidad = parseInt(prompt("Cuántos Ficus Hawaii deseas? (Stock máximo 30)"));
+                        break;
 
-                        if (cantidad < 30) {
-                            alert("Agregaste al carrito " + cantidad + " Ficus Hawaii");
-                            alert("Precio final: $" + (730 * cantidad));
+                    case 2:
+                        alert("Seleccionaste Articulo Ficus Hawaii. Precio: $730");
+
+                        let cantidadFicusHawaii = parseInt(prompt("Cuántos Ficus Hawaii deseas? (Stock máximo 30)"));
+
+                        if (cantidadFicusHawaii < 30) {
+                            alert("Agregaste al carrito " + cantidadFicusHawaii + " Ficus Hawaii");
+                            alert("Precio final: $" + (730 * cantidadFicusHawaii));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarFicus();
-                    break;
-                case 3:
-                    alert("Seleccionaste Articulo Gomero Rubra. Precio: $1530");
-                    function comprarGomero() {
-                        let cantidad = parseInt(prompt("Cuántos Gomeros Rubra deseas? (Stock máximo 20)"));
 
-                        if (cantidad < 20) {
-                            alert("Agregaste al carrito " + cantidad + " Gomeros Rubra");
-                            alert("Precio final: $" + (1530 * cantidad));
+                        break;
+                    case 3:
+                        alert("Seleccionaste Articulo Gomero Rubra. Precio: $1530");
+
+                        let cantidadGomeroRubra = parseInt(prompt("Cuántos Gomeros Rubra deseas? (Stock máximo 20)"));
+
+                        if (cantidadGomeroRubra < 20) {
+                            alert("Agregaste al carrito " + cantidadGomeroRubra + " Gomeros Rubra");
+                            alert("Precio final: $" + (1530 * cantidadGomeroRubra));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarGomero();
-                    break;
-                default:
-                    alert("Ingresá un producto válido.Opción 1 Orquidea Phalaenopsis , Opcion 2 Ficus Hawaii y Opción 3 Gomero Rubra.");
-            }
-            break;
-        case 2:
-            let prodMacetas = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Maceta Rotoldeadas , Opcion 2 Macetas animales y Opción 3 Maceta Terracota Nro14. "));
-            switch (prodMacetas) {
-                case 1:
-                    alert("Seleccionaste Articulo Maceta Rotoldeadas. Precio: $845");
-                    function comprarMacetasRotoldeadas() {
-                        let cantidad = parseInt(prompt("Cuántas Maceta Rotoldeadas deseas? (Stock máximo 40)"));
 
-                        if (cantidad < 40) {
-                            alert("Agregaste al carrito " + cantidad + " Maceta Rotoldeadas");
-                            alert("Precio final: $" + (845 * cantidad));
+                        break;
+                    case 0:
+                        alert("Gracias, esperamos que vuelvas prontos!")
+                        break;
+                    default:
+                        alert("Ingresá un producto válido.Opción 1 Orquidea Phalaenopsis , Opcion 2 Ficus Hawaii y Opción 3 Gomero Rubra. Si deseas salir del proceso de compra, presioná 0 ");
+                }
+                break;
+            case 2:
+                let prodMacetas = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Maceta Rotoldeadas , Opcion 2 Macetas animales y Opción 3 Maceta Terracota Nro14. Si deseas salir del proceso de compra, presioná 0 "));
+                switch (prodMacetas) {
+
+                    case 1:
+                        alert("Seleccionaste Articulo Maceta Rotoldeadas. Precio: $845");
+                        let cantidadMacetaRotoldeadas = parseInt(prompt("Cuántas Maceta Rotoldeadas deseas? (Stock máximo 40)"));
+
+                        if (cantidadMacetaRotoldeadas < 40) {
+                            alert("Agregaste al carrito " + canticantidadMacetaRotoldeadasdad + " Maceta Rotoldeadas");
+                            alert("Precio final: $" + (845 * cantidadMacetaRotoldeadas));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarMacetasRotoldeadas();
-                    break;
 
-                case 2:
-                    alert("Seleccionaste Articulo Macetas animales. Precio: $1245");
-                    function comprarMacetasAnimales() {
-                        let cantidad = parseInt(prompt("Cuántas Macetas animales deseas? (Stock máximo 40)"));
+                        break;
 
-                        if (cantidad < 40) {
-                            alert("Agregaste al carrito " + cantidad + " Macetas animales");
-                            alert("Precio final: $" + (1245 * cantidad));
+                    case 2:
+                        alert("Seleccionaste Articulo Macetas animales. Precio: $1245");
+
+                        let cantidadMacetasAnimales = parseInt(prompt("Cuántas Macetas animales deseas? (Stock máximo 40)"));
+
+                        if (cantidadMacetasAnimales < 40) {
+                            alert("Agregaste al carrito " + cantidadMacetasAnimales + " Macetas animales");
+                            alert("Precio final: $" + (1245 * cantidadMacetasAnimales));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarMacetasAnimales();
-                    break;
-                case 3:
-                    alert("Seleccionaste Articulo Maceta Terracota Nro14. Precio: $285");
-                    function comprarMacetasTerracotaNro14() {
-                        let cantidad = parseInt(prompt("Cuántas Maceta Terracota Nro14 deseas? (Stock máximo 40)"));
 
-                        if (cantidad < 40) {
-                            alert("Agregaste al carrito " + cantidad + " Maceta Terracota Nro14");
-                            alert("Precio final: $" + (285 * cantidad));
+                        break;
+                    case 3:
+                        alert("Seleccionaste Articulo Maceta Terracota Nro14. Precio: $285");
+
+                        let cantidadMacetasTerracotanro14 = parseInt(prompt("Cuántas Maceta Terracota Nro14 deseas? (Stock máximo 40)"));
+
+                        if (cantidadMacetasTerracotanro14 < 40) {
+                            alert("Agregaste al carrito " + cantidadMacetasTerracotanro14 + " Maceta Terracota Nro14");
+                            alert("Precio final: $" + (285 * cantidadMacetasTerracotanro14));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarMacetasTerracotaNro14();
-                    break;
-                default:
-                    alert("Ingresá un producto válido.Opción 1 Maceta Rotoldeadas , Opcion 2 Macetas animales y Opción 3 Maceta Terracota Nro14.");
-            }
-            break;
-        case 3:
-            let prodAccesorios = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Tutores de caña fina , Opcion 2 Figuras de mariposas decorativas y Opción 3 Estanteria para plantas. "));
-            switch (prodAccesorios) {
-                case 1:
-                    alert("Seleccionaste Articulo Tutores de caña fina. Precio: $645");
-                    function comprarTutotes() {
-                        let cantidad = parseInt(prompt("Cuántos Tutores de caña fina deseas? (Stock máximo 30)"));
 
-                        if (cantidad < 30) {
-                            alert("Agregaste al carrito " + cantidad + " Tutores de caña fina");
-                            alert("Precio final: $" + (645 * cantidad));
+                        break;
+                    case 0:
+                        alert("Gracias, esperamos que vuelvas prontos!")
+                        break;
+                    default:
+                        alert("Ingresá un producto válido.Opción 1 Maceta Rotoldeadas , Opcion 2 Macetas animales y Opción 3 Maceta Terracota Nro14. Si deseas salir del proceso de compra, presioná 0 ");
+                }
+                break;
+            case 3:
+                let prodAccesorios = parseInt(prompt("Accedé al producto que más te guste : Opción 1 Tutores de caña fina , Opcion 2 Figuras de mariposas decorativas y Opción 3 Estanteria para plantas. Si deseas salir del proceso de compra, presioná 0 "));
+                switch (prodAccesorios) {
+
+                    case 1:
+                        alert("Seleccionaste Articulo Tutores de caña fina. Precio: $645");
+
+                        let cantidadTutores = parseInt(prompt("Cuántos Tutores de caña fina deseas? (Stock máximo 30)"));
+
+                        if (cantidadTutores < 30) {
+                            alert("Agregaste al carrito " + cantidadTutores + " Tutores de caña fina");
+                            alert("Precio final: $" + (645 * cantidadTutores));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarTutotes();
-                    break;
 
-                case 2:
-                    alert("Seleccionaste Articulo Figuras de mariposas decorativas. Precio: $455");
-                    function comprarFigurasMariposas() {
-                        let cantidad = parseInt(prompt("Cuántos Figuras de mariposas decorativas deseas? (Stock máximo 30)"));
+                        break;
 
-                        if (cantidad < 30) {
-                            alert("Agregaste al carrito " + cantidad + " Figuras de mariposas decorativas");
-                            alert("Precio final: $" + (455 * cantidad));
+                    case 2:
+                        alert("Seleccionaste Articulo Figuras de mariposas decorativas. Precio: $455");
+
+                        let cantidadFigurasMariposas = parseInt(prompt("Cuántos Figuras de mariposas decorativas deseas? (Stock máximo 30)"));
+
+                        if (cantidadFigurasMariposas < 30) {
+                            alert("Agregaste al carrito " + cantidadFigurasMariposas + " Figuras de mariposas decorativas");
+                            alert("Precio final: $" + (455 * cantidadFigurasMariposas));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarFigurasMariposas();
-                    break;
-                case 3:
-                    alert("Seleccionaste Articulo Estanteria para plantas. Precio: $2285");
-                    function comprarEstanteria() {
-                        let cantidad = parseInt(prompt("Cuántos Estanteria para plantas deseas? (Stock máximo 20)"));
 
-                        if (cantidad < 20) {
-                            alert("Agregaste al carrito " + cantidad + " Estanteria para plantas");
-                            alert("Precio final: $" + (2285 * cantidad));
+                        break;
+                    case 3:
+                        alert("Seleccionaste Articulo Estanteria para plantas. Precio: $2285");
+
+                        let cantidadEstanteria = parseInt(prompt("Cuántos Estanteria para plantas deseas? (Stock máximo 20)"));
+
+                        if (cantidadEstanteria < 20) {
+                            alert("Agregaste al carrito " + cantidadEstanteria + " Estanteria para plantas");
+                            alert("Precio final: $" + (2285 * cantidadEstanteria));
                         } else {
-                            alert("Lo sentimos, no contamos con stock");
+                            alert("Lo sentimos, no contamos con esa cantidad en stock por el momento.");
                         };
-                    };
-                    comprarEstanteria();
-                    break;
-                default:
-                    alert("Ingresá un producto válido.Opción 1 Tutores de caña fina , Opcion 2 Figuras de mariposas decorativas y Opción 3 Estanteria para plantas.");
-            }
-            break;
-        default:
-            alert("Categoría no válida. Debes elegir entre las siguientes: Opción 1 Categoría: Plantas, Opcion 2 Categoría: Macetas y Opción 3 Categoría: Accesorios.");
+
+                        break;
+                    case 0:
+                        alert("Gracias, esperamos que vuelvas prontos!")
+                        break;
+                    default:
+                        alert("Ingresá un producto válido.Opción 1 Tutores de caña fina , Opcion 2 Figuras de mariposas decorativas y Opción 3 Estanteria para plantas. Si deseas salir del proceso de compra, presioná 0 ");
+                }
+                break;
+            default:
+                alert("Categoría no válida. Debes elegir entre las siguientes: Opción 1 Categoría: Plantas, Opcion 2 Categoría: Macetas y Opción 3 Categoría: Accesorios. En caso que desees salir, presioná la opcion 0");
+        }
+        seleccionarCateg = parseInt(prompt("Accedé nuevamente a la categoría: Opción 1 Categoría: Plantas, Opción 2 Categoría: Macetas, Opción 3 Categoría: Accesorios. Si deseas salir, presioná 0"));
     }
-
 }
-else if (continuarCompra === 2){
+else if (continuarCompra === 2) {
     alert("Has salido del proceso de compra. ¡Esperamos que vuelvas pronto!");
 }
 else {
     parseInt(prompt("Debes seleccionar una opcion correcta. Opción 1 para continuar con la compra. Opcion 2 para salir del proceso de compra."));
 }
-
